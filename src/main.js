55c27084e3b88bf1e55d488f3715a715cc864098
gursel <gtzer75@gmail.com>
2018-09-29 12:33:53 +0300

    Default Changelist

--- src/main.js
+++ src/main.js
@@ -9,45 +9,47 @@ import ConnectSpotify from './components/ConnectSpotify.vue'
 import PlaylistProps from './components/PlaylistProps.vue'
 
 const router = new VueRouter({
-	routes: [
-	{ path: '/', component: ConnectSpotify, name: 'connectSpotify' },
-	{ path: '/playlist', component: PlaylistProps, name: 'playlistProps' },
-	{ path: '/callback', component: Callback, name: 'callback' }
-	],
-	mode: 'hash'
+  routes: [
+    {path: '/', component: ConnectSpotify, name: 'connectSpotify'},
+    {path: '/playlist', component: PlaylistProps, name: 'playlistProps'},
+    {path: '/callback', component: Callback, name: 'callback'}
+  ],
+  mode: 'hash'
 });
 
 router.beforeEach(function (to, from, next) {
-	if (to.path === '/playlist') {
-		if(localStorage.getItem("accessToken")){
-			next();
-		} else {
-			next("/");
-		}
-	} else {
-		if(to.path.includes('/access_token')){
+  if (to.path === '/playlist') {
+    if (localStorage.getItem("accessToken")) {
+      next();
+    } else {
+      next("/");
+    }
+  } else {
+    if (to.path.includes('/access_token')) {
 
-			localStorage.setItem("accessToken", window.location.hash.split('&')[0].split("#/access_token=")[1]);
-			localStorage.setItem("tokenTime", window.location.hash.split('&')[2].split("expires_in=")[1]);
-			fetch("https://api.spotify.com/v1/me",{
-				headers: {
-					'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
-				}
-			})
-			.then((res) => { return res.json() })
-			.then((res) => {
-				localStorage.setItem("user", JSON.stringify(res));
-				next("/playlist");
-			});
-			
-		} else {
-			next();
-		}
-	}
+      localStorage.setItem("accessToken", window.location.hash.split('&')[0].split("#/access_token=")[1]);
+      localStorage.setItem("tokenTime", window.location.hash.split('&')[2].split("expires_in=")[1]);
+      fetch("https://api.spotify.com/v1/me", {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
+        }
+      })
+        .then((res) => {
+          return res.json()
+        })
+        .then((res) => {
+          localStorage.setItem("user", JSON.stringify(res));
+          next("/playlist");
+        });
+
+    } else {
+      next();
+    }
+  }
 });
 
 new Vue({
-	el: '#app',
-	render: h => h(App),
-	router
+  el: '#app',
+  render: h => h(App),
+  router
 })
